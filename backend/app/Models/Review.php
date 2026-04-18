8a8dfec612d1325bd70b3065c1b3abbceec6791c
Ivan-del3 <nagatowork3@gmail.com>
2026-04-18 19:16:53 +0200

    Models Notificarion, Report, Review

---
+++ backend/app/Models/Review.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    protected $fillable = ['order_id', 'user_id', 'reviewed_user_id', 'rating', 'comment'];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reviewedUser() {
+        return $this->belongsTo(User::class, 'reviewed_user_id');
+    }
+}
