5ae1eceb869a342a59be37f9df47b2dd36fa098a
Ivan-del3 <nagatowork3@gmail.com>
2026-05-16 13:23:27 +0200

    wrap app with NotificationsProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
 import { AuthModalProvider } from './context/AuthModalContext'
+import { NotificationsProvider } from './context/NotificationsContext'
 import App from './App.jsx'
 import './styles/index.css'
 
@@ -9,7 +10,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <AuthModalProvider>
-        <App />
+        <NotificationsProvider>
+          <App />
+        </NotificationsProvider>
       </AuthModalProvider>
     </AuthProvider>
   </StrictMode>
